b6e56c03841ce59f439b8ce11eb146346ff95e30
IdkWhatUsername2do <szlgm2018@gmail.com>
2025-10-13 03:14:11 -0400

    Added second page and return button

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const Title = document.querySelector(".TitleText")
 const Cursor = document.querySelector(".cursor")
+const Background = document.querySelector(".bg")
+const Background2 = document.querySelector(".bg2")
+const PortfolioPage = document.querySelector(".PortfolioPage")
+const ReturnButton = document.querySelector(".Return")
 
-document.addEventListener('mousemove', (e)=>{
-    Cursor.style.transform = "translate(" + e.clientX + "px" + "," + e.clientY +"px)";
-    Cursor.style.left = "-5px";
-    Cursor.style.top = "-5px";
-})
-Title.addEventListener('mousemove',(e)=>{
+function hoverover(e){
     Cursor.style.width = "30px";
     Cursor.style.height = "30px";
     Cursor.style.backgroundColor = "rgba(123,231,132,1)";
@@ -18,11 +17,41 @@ Title.addEventListener('mousemove',(e)=>{
         Title.classList.add("from-right");
         Title.classList.remove("from-left");
     }
-})
-Title.addEventListener('mouseleave', () => {
+}
+function hoverleave(){
     Cursor.style.width = "15px";
     Cursor.style.height = "15px";
     Cursor.style.background = "rgba(255,140,0,0.75)";
     Title.classList.remove("from-left","from-right");
-});
+}
+document.addEventListener('mousemove', (e)=>{
+
+    Cursor.style.left = e.clientX - 7.5+ "px";
+    Cursor.style.top = e.clientY  -7.5 + "px";
+ 
+    
+})
+Title.addEventListener('mousemove',hoverover);
+ReturnButton.addEventListener('click', ()=>{
+    Background.style.opacity = 1;
+    Background.style.pointerEvents = "auto";
+    PortfolioPage.style.opacity = 0;
+    PortfolioPage.style.pointerEvents = "none";
+    Title.style.opacity = 1;
+    Title.style.pointerEvents = "auto";
+
+})
+    
+Title.addEventListener('click', ()=>{
+    Background.style.opacity = 0;
+    Background.style.pointerEvents = "none";
+    PortfolioPage.style.opacity = 1;
+    PortfolioPage.style.pointerEvents = "auto";
+    Title.style.opacity = 0;
+    Title.style.pointerEvents = "none";
+    
+
+})
+
+Title.addEventListener('mouseleave', hoverleave);
 
