62821019ae266731158d4be551b5cac9b0731efa
IdkWhatUsername2do <szlgm2018@gmail.com>
2025-10-14 02:14:58 -0400

    Started the portfolio page

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const Background = document.querySelector(".bg")
 const Background2 = document.querySelector(".bg2")
 const PortfolioPage = document.querySelector(".PortfolioPage")
 const ReturnButton = document.querySelector(".Return")
-
+const PortfolioHolder = document.querySelectorAll(".PortfolioHolder")
+let IsInFocusMode = false;
+let OneInFocusMode;
 function hoverover(e){
     Cursor.style.width = "30px";
     Cursor.style.height = "30px";
@@ -31,27 +33,44 @@ document.addEventListener('mousemove', (e)=>{
  
     
 })
-Title.addEventListener('mousemove',hoverover);
+
 ReturnButton.addEventListener('click', ()=>{
     Background.style.opacity = 1;
     Background.style.pointerEvents = "auto";
     PortfolioPage.style.opacity = 0;
+    PortfolioHolder.forEach(el=>{
+        el.style.opacity = 0;
+        el.style.pointerEvents = "none";
+    });
+    IsInFocusMode = false;
     PortfolioPage.style.pointerEvents = "none";
     Title.style.opacity = 1;
     Title.style.pointerEvents = "auto";
 
+
 })
+
+PortfolioHolder.forEach(el=>{
     
+})
+
 Title.addEventListener('click', ()=>{
     Background.style.opacity = 0;
     Background.style.pointerEvents = "none";
     PortfolioPage.style.opacity = 1;
+    
+    PortfolioHolder.forEach(el=>{
+        el.style.opacity=1;
+        el.style.pointerEvents = "auto";
+    });
     PortfolioPage.style.pointerEvents = "auto";
     Title.style.opacity = 0;
     Title.style.pointerEvents = "none";
     
 
 })
-
+Title.addEventListener('mousemove',hoverover);
 Title.addEventListener('mouseleave', hoverleave);
 
+ReturnButton.addEventListener('mousemove',hoverover);
+ReturnButton.addEventListener('mouseleave',hoverleave);
